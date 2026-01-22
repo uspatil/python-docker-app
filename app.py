e9b6bf73cb07f161f77ace78a9d45ab0303b56a5
umeshpatil03 <umeshpatil03@yahoo.com>
2026-01-22 23:37:21 +0530

    added test route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "🚀 Python App Deployed using Docker & GitHub Actions! is working now"
 
+@app.route("/test")
+def hello():
+    return "🚀 test route working fine"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
