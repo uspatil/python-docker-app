9f811baea7fd77f4fb5eac10f47080556adc50cb
umeshpatil03 <umeshpatil03@yahoo.com>
2026-01-23 15:09:58 +0530

    added fast api code and react frontend along with mysql

---
+++ frontend/src/UserList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function UserList({ users, onEdit, onDelete }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>
+              <button className="edit" onClick={() => onEdit(user)}>
+                Edit
+              </button>
+              <button className="delete" onClick={() => onDelete(user.id)}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default UserList;
